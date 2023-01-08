bf3d17e3294f0f995a742681d446ff3561c12a88
kasvrol <dovalekarolina@gmail.com>
2023-01-08 20:13:38 -0300

    feat: cap 1 javascript - uma visão geral rápida => Programação orientada a objetos em Javascript

---
+++ src/assets/dia01aoDia10/quartoDia.js
@@ -0,0 +1,74 @@
+//Programação orientada a objetos em javascript
+
+/*formas de declarar objetos*/
+var obj = new Object();
+var obj2 = {};
+
+obj2 = {
+    name: /*chave do objeto */ {
+        first: /*chave do objeto */ "Gandalf" /**valor do objeto*/,
+        last: /*chave do objeto */ "the Gray" /**valor do objeto*/,
+        /**valor do objeto*/
+    },
+    adress: /*chave do objeto */ "Middle Earth" /**valor do objeto*/,
+};
+
+/*envio para o arquivo index*/
+function objetos() {
+    return {
+        obj: obj,
+        obj2: obj2,
+    };
+}
+
+/*export default objetos();
+envio para o arquivo index*/
+
+/**
+ *objeto é instância de uma classe
+ *uma classe define as características do objeto
+ */
+
+/*modos de declarar classes: */
+function Book(titulo, paginas, isbn) {
+    this.titulo = titulo;
+    this.paginas = paginas;
+    this.isbn = isbn;
+}
+
+var book = new Book("contos extraordinários", "300", "00000");
+
+/*export default book;
+envio para o arquivo index
+
+log:
+Book {
+  titulo: 'contos extraordinários',
+  paginas: '300',
+  isbn: '00000'
+} */
+
+/**atualizar propriedades:*/
+book.titulo = "alice no país das maravilhas";
+/*export default book;
+envio para o arquivo index
+
+log:
+Book {
+    titulo: 'alice no país das maravilhas',
+    paginas: '300',
+    isbn: '00000'
+  } */
+
+/**funções/métodos:*/
+Book.prototype.printTitle = function () {
+    console.log(this.titulo);
+};
+
+/*export default book.printTitle(); 
+envio para o arquivo index
+
+log:
+alice no país das maravilhas
+ */
+export default book.printTitle();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import segundoDia from "./assets/dia01aoDia10/segundoDia.js";
+import quartoDia from "./assets/dia01aoDia10/quartoDia.js";
 
 function returnLogs() {
-    console.log(segundoDia)
+    //console.log(segundoDia)
+    //console.log(quartoDia);
+    quartoDia; /*para book.printTitle(); */
 }
 
-returnLogs()
-
+returnLogs();
