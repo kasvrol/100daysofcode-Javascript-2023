f74a0c483b107d9d8d7451a4bc3b7c86c878ef57
kasvrol <dovalekarolina@gmail.com>
2023-03-13 01:05:43 -0300

    importação de logs

---
+++ src/assets/importacaoDia11AoDia20.js
@@ -0,0 +1,16 @@
+import dia11 from "./dia11AoDia20/dia11.js";
+import dia11PartII from "./dia11AoDia20/dia11PartII.js";
+import dia12 from "./dia11AoDia20/dia12.js";
+import dia12PartII from "./dia11AoDia20/dia12PartII.js";
+import dia13 from "./dia11AoDia20/dia13.js";
+import dia13PartII from "./dia11AoDia20/dia13PartII.js";
+function returnLogs() {
+    //dia11();
+    //dia11PartII()
+    //dia12();
+    //dia12PartII();
+    //dia13();
+    dia13PartII();
+}
+
+export default returnLogs;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import importacaoDia01AoDia10 from "./assets/importacaoDia01AoDia10.js";
+import importacaoDia11AoDia20 from "./assets/importacaoDia11AoDia20.js";
 
 function returnLogs() {
-    importacaoDia01AoDia10();
+    //importacaoDia01AoDia10();
+    importacaoDia11AoDia20();
 }
 
 returnLogs();
